86824f07e73b0e86da238dbbd70a9d5a19f37e93
cedriclenders <32706636+cedriclenders@users.noreply.github.com>
2019-03-25 14:53:50 +0100

    date difference method added

---
+++ Brexit Django/Brexit/brexitdays/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from datetime import datetime
+
+
+def index(request):
+
+    brexit = datetime(2019, 3, 29, 23, 0, 0)
+    diff = brexit - datetime.now()
+    print(diff)
+    return render(request, 'brexit/index.html', {'total_days': diff})
